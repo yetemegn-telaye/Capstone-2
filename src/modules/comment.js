205ceeab050662e25684707d5b91e68962e782c2
Irirwanirira <irijoseph41@gmail.com>
2022-10-28 10:20:12 +0200

    Modified comment.js

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,41 +1,37 @@
 const commentUrl = ('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eC8v1nWSCKC4m4T5bPlw/comments/');
 
+const getComments = async (item_id) => {
+  const comments = await fetch(`${commentUrl}?item_id=${item_id}`);
+  const commentsData = await comments.json();
+  return commentsData;
+};
 
-const getComments = async (item_id)=>{
-    const comments = await fetch(`${commentUrl}?item_id=${item_id}`);
-    const commentsData = await comments.json();
-    return commentsData;
-}
+const countComments = (commentsArray) => {
+  let countComment = 0;
+  commentsArray.forEach((comment) => {
+    countComment++;
+  });
+  return countComment;
+};
 
-const countComments = (commentsArray)=>{
-    let countComment = 0;
-    commentsArray.forEach(comment => {
-        countComment++;
-    });
-    return countComment;
-}
+const addComment = async (item1, name, message) => {
+  await fetch(commentUrl, {
 
-const addComment = async (item1,name,message)=>{
-    await fetch(commentUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: item1,
+      username: name,
+      comment: message,
 
-        method: 'POST',
-        body: JSON.stringify({
-          item_id: item1,
-          username: name,
-          comment: message
+    }),
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+  });
 
-        }),
-        headers: {
-          'content-type': 'application/json; charset=UTF-8',
-        },
-      });
-     
-       
-      
-      const commentsArray = await getComments(item1);
-      countComments(commentsArray);
-      return commentsArray;
-      
-}
+  const commentsArray = await getComments(item1);
+  countComments(commentsArray);
+  return commentsArray;
+};
 
-export {addComment,getComments,countComments};
+export { addComment, getComments, countComments };
